05379d680bb1537a47f0d54dfb39371b8f09c88c
Bagus Rian B <magusrian09@gmail.com>
2026-05-11 08:47:03 +0700

    fix: Penyesuai router web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,86 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\AdminController;
 
-// Homepage
-Route::get('/', [PostController::class, 'index']);
+use App\Http\Controllers\User\HomeController;
+use App\Http\Controllers\User\PostController;
+use App\Http\Controllers\User\CategoryController;
 
-// Detail berita
-Route::get('/post/{id}', [PostController::class, 'show']);
+use App\Http\Controllers\Admin\PostController as AdminPostController;
+use App\Http\Controllers\Admin\DashboardController;
 
-// Admin
-Route::get('/admin/create', [AdminController::class, 'create']);
-Route::post('/admin/store', [AdminController::class, 'store']);
-Route::get('/admin/edit/{id}', [AdminController::class, 'edit']);
-Route::post('/admin/update/{id}', [AdminController::class, 'update']);
-Route::post('/admin/delete/{id}', [AdminController::class, 'destroy']);
+//
+// USER
+//
+
+// HOME
+Route::get(
+    '/',
+    [HomeController::class, 'index']
+)->name('home');
+
+// DETAIL ARTICLE
+Route::get(
+    '/post/{id}',
+    [PostController::class, 'show']
+)->name('post.show');
+
+// CATEGORY
+Route::get(
+    '/category/{slug}',
+    [CategoryController::class, 'show']
+)->name('category.show');
+
+//
+// ADMIN
+//
+
+Route::prefix('admin')->group(function () {
+
+    // DASHBOARD
+    Route::get(
+        '/dashboard',
+        [DashboardController::class, 'index']
+    )->name('admin.dashboard');
+
+    //
+    // POSTS
+    //
+
+    // INDEX
+    Route::get(
+        '/posts',
+        [AdminPostController::class, 'index']
+    )->name('admin.posts.index');
+
+    // CREATE
+    Route::get(
+        '/posts/create',
+        [AdminPostController::class, 'create']
+    )->name('admin.posts.create');
+
+    // STORE
+    Route::post(
+        '/posts/store',
+        [AdminPostController::class, 'store']
+    )->name('admin.posts.store');
+
+    // EDIT
+    Route::get(
+        '/posts/edit/{id}',
+        [AdminPostController::class, 'edit']
+    )->name('admin.posts.edit');
+
+    // UPDATE
+    Route::post(
+        '/posts/update/{id}',
+        [AdminPostController::class, 'update']
+    )->name('admin.posts.update');
+
+    // DELETE
+    Route::get(
+        '/posts/delete/{id}',
+        [AdminPostController::class, 'destroy']
+    )->name('admin.posts.delete');
+
+});
