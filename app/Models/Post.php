c22f4fdffaaa44343855e8f795faa6785682a22e
Bagus Rian B <magusrian09@gmail.com>
2026-05-11 09:01:29 +0700

    Fix: models post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,15 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+
 
 class Post extends Model
 {
     protected $table = 'posts';
 
+    protected $primaryKey = 'id_posts';
+
     protected $fillable = [
-    'title',
-    'content',
-    'image',
-    'categories_id'
+        'title',
+        'slug',
+        'content',
+        'image',
+        'status',
+        'categories_id'
     ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | RELATION CATEGORY
+    |--------------------------------------------------------------------------
+    */
+
+    public function category()
+    {
+        return $this->belongsTo(
+            Category::class,
+            'categories_id',
+            'id_categories'
+        );
+    }
+
+    
 }
